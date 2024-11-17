76190a6e2a8dc14906710f663c5f4fe968b0428b
DJ3125 <dylanalexja@gmail.com>
2024-11-17 10:06:10 -0600

    Finished Product

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {useEffect, useState} from 'react';
+
+import {DeviceMotion} from 'expo-sensors';
+
 export default function App() {
+  const [rotationInfo, setRotationInfo] = useState({beta: 0, gamma: 0});
+
+  useEffect(function(){
+    DeviceMotion.setUpdateInterval(100);
+    DeviceMotion.addListener(function({rotation: {beta, gamma}}){
+      setRotationInfo({
+        beta: beta,
+        gamma: gamma,
+      });      
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>X axis: {rotationInfo.beta}</Text>
+      <Text>Y axis: {rotationInfo.gamma}</Text>
       <StatusBar style="auto" />
     </View>
   );
